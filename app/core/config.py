59f7a52cf41b4cb47fe8aa6fd3f6234e44038206
smallziz <abdaziz.diouf@astarex-ps.com>
2026-01-24 09:12:15 +0000

    update configuration and add health check test

--- app/core/config.py
+++ app/core/config.py
@@ -11,14 +11,16 @@ class Settings(BaseSettings):
     ENVIRONMENT: Literal["dev", "prod"] = "dev"
     model_config = SettingsConfigDict(
         env_file=ENV_FILE,
+        env_file_encoding="utf-8"
     )
-    API_V1_STR: str = ""
-    PROJECT_NAME: str = ""
-    PROJECT_DESCRIPTION: str = ""
-    SITE_NAME: str = ""
-    DATABASE_URL: str = ""
-    MAIL_FROM: str = ""
-    MAIL_FROM_NAME: str = ""
+
+    API_V1_STR: str = "/api/v1"
+    PROJECT_NAME: str = "My Blog API"
+    PROJECT_DESCRIPTION: str = "Backend API for my blog"
+    SITE_NAME: str = "SMALLZIZ'S BLOG"
+    DATABASE_URL: str = "postgresql+asyncpg://smallziz:dabzo123@postgres:5432/nextgen"
+    MAIL_FROM: str = "noreply@gmail.com"
+    MAIL_FROM_NAME: str = "SMALLZIZ'S BLOG"
     SMTP_HOST: str = "mailpit"
     SMTP_PORT: int = 1025
     SMTP_UI_PORT: int = 8025
